6937ee05863a9314ccc6e2355dbc2152d2926905
Roger <rhoang510@gmail.com>
2021-12-03 12:17:45 -0800

    updates

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
 const container = document.querySelector("#container");
 const clearGrid = document.querySelector(".clear");
-const box = document.querySelector(".grid-item");
 
-function makeGrid(rows, cols) {
-    container.style.setProperty("--grid-rows", rows);
-    container.style.setProperty("--grid-cols", cols);
-    for (i = 0; i < (rows * cols); i++) {
-        let cell = document.createElement("div");
-        container.appendChild(cell).className = "grid-item";
+makeGrid(16);
+
+function makeGrid(gridSize) {
+    container.style.setProperty("--columns", gridSize);
+    container.style.setProperty("--rows", gridSize);
+    for (i = 0; i < (gridSize ** 2); i++) {
+        const div = document.createElement("div");
+        div.classList.add("box");
+        container.appendChild(div);
     };
 };
 
-makeGrid(16, 16);
+const boxes = document.querySelectorAll(".box");
 
-container.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = "red";
+boxes.forEach((box) => {
+    box.addEventListener ("mouseover", () => {
+        box.style.backgroundColor = "red";
+    });
 });
 
 clearGrid.addEventListener("click", () => {
-    box.forEach
+    boxes.forEach((box) => {
+        box.remove();
+    });
+    changeGrid();
 });
+
+function changeGrid() {
+    let changeGrid = parseInt(prompt("Enter the size of grid you want. (2-100)", ""));
+    let gridSize = changeGrid;
+    if (changeGrid > 1 && changeGrid <= 100) {
+         makeGrid(gridSize);
+    } else {
+         alert("Please choose a number between 2 and 100.");
+    }
+}
