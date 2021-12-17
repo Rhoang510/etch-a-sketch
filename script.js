7b5e28e91e8ddc330395ffa9feb291eb4e5ceca5
Roger <rhoang510@gmail.com>
2021-12-17 08:53:00 -0800

    Added few features

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const rgb = document.querySelector(".rgb");
 const black = document.querySelector(".black");
 const changeSize = document.querySelector(".changeSize");
 const size = document.querySelector(".size");
+const canvas = document.querySelector(".canvas");
 const colorPicker = document.querySelector("#colorPicker");
 const slider = document.querySelector("#slider");
+const sizeSlider = document.querySelectorAll("#sizeSlider")
 let color = "black";
-let pen = false;
+let brush = false;
 
 function makeGrid(gridSize) {
     container.style.setProperty("--columns", gridSize);
@@ -17,11 +19,27 @@ function makeGrid(gridSize) {
         div.classList.add("box");
         container.appendChild(div);
     };
-        container.addEventListener("click", togglePen);
+        container.addEventListener("click", toggleBrush);
 };
 
 makeGrid(16);
 size.textContent = "Size: 16 x 16";
+canvas.textContent = "Canvas Size: 540 x 540"
+
+function toggleBrush() {
+    const boxes = document.querySelectorAll(".box");
+    if (!brush) {
+        boxes.forEach((box) => {
+            box.addEventListener("mouseover", changeColor);
+        });
+        brush = true;
+    } else {
+        boxes.forEach((box) => {
+            box.removeEventListener("mouseover", changeColor);
+        });
+        brush = false;
+    }
+}
 
 function changeColor() {
     switch (color) {
@@ -37,21 +55,7 @@ function changeColor() {
   }
 }
 
-function togglePen() {
-    const boxes = document.querySelectorAll(".box");
-    if (!pen) {
-        boxes.forEach((box) => {
-            box.addEventListener("mouseleave", changeColor);
-        });
-        pen = true;
-    } else {
-        boxes.forEach((box) => {
-            box.removeEventListener("mouseleave", changeColor);
-        });
-        pen = false;
-    }
-}
-
+// Event Listeners
 rgb.addEventListener("click", () => { color = "rgb"; });
 black.addEventListener("click", () => { color = "black"; });
 colorPicker.addEventListener("click", () => { color = "userPick"; });
@@ -70,5 +74,11 @@ slider.addEventListener("change", () => {
     });
     makeGrid(slider.value);
     size.textContent = `Size: ${slider.value} x ${slider.value}`;
+});
+
+sizeSlider.addEventListener("change", () => {
+    container.style.height = sizeSlider.value;
+    container.style.width = sizeSlider.value;
+    canvas.textContent = `Canvas Size: ${sizeSlider.value} x ${sizeSlider.value}`;
 });
 
